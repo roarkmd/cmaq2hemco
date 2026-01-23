cfd46a484e5bd58882ea9c40b3e2484d3147e58c
roarkmd <roark.matthew@epa.gov>
2026-01-23 14:18:29 -0500

    Added scipy as a requirement.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        "pyproj", "pandas", "xarray", "requests", "cmaqsatproc", "netcdf4"
+        "pyproj", "pandas", "xarray", "requests", "cmaqsatproc", "netcdf4", "scipy",
     ],
     extras_require={
         'aws': [
